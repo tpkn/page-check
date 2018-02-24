cc518986b813e12f252b4035863dc3b68a6bed58
tpkn <tpkn@users.noreply.github.com>
2018-02-24 20:05:29 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,107 @@
-# page-check
+# Page Check
 Webpage tester based on PhantomJS and Node.js
+
+
+
+Module runs your webpage, app or banner within PhantomJS. While running, it registers next few things:
+- javascript errors (including errors inside `window.onresize` handler)
+- missing files
+- console events (`.log()`, `.warn()`, etc.)
+- requesting files from different host
+
+
+## Usage
+```javascript
+const PageCheck = require('page-check');
+
+PageCheck('http://localhost:8000/test')
+.then(result => {
+   console.log(JSON.stringify(result, true, 2));
+}, err => {
+   console.log(JSON.stringify(err, true, 2));
+});
+```
+
+
+
+## Options
+
+### link 
+__Type__: *String*<br>
+
+
+### options 
+__Type__: *Object*, *Number*<br>
+
+If a `Number`, then acts like `timeout` option
+
+
+### options.filter
+__Type__: *Function*<br>
+
+This function will be used by `Array.filter()` to filter final list of errors.<br>
+For example we could leave only `js errors` and `missing files`:<br>
+
+```javascript
+PageCheck('http://localhost:8000/test', {
+   filter: err => /[12]/.test(err.code)
+})
+```
+
+
+
+### options.no_clones
+__Type__: *Boolean*<br>
+__Default__: *false*<br> 
+
+If set to `true`, the same errors will be removed from the list
+
+
+### timeout 
+__Type__: *Number*<br>
+__Default__: `60`<br>
+
+Keep checking for this amout of seconds
+
+
+
+
+## Output
+```
+{
+  "status": "done",
+  "link": "http://localhost:8000/test",
+  "errors": [
+    {
+      "code": 1,
+      "type": "js error",
+      "details": "ReferenceError: Can't find variable: git",
+      "line": "line 138"
+    },
+    {
+      "code": 2,
+      "type": "missing file",
+      "details": "Error downloading http://localhost:8000/test/style.css",
+      "line": ""
+    },
+    {
+      "code": 3,
+      "type": "console",
+      "details": "yey!",
+      "line": ""
+    },
+    {
+      "code": 4,
+      "type": "external request",
+      "details": "https://google.com/logo.png",
+      "line": ""
+    }
+  ]
+}
+```
+
+
+
+
+
+
---
+++ page-check.js
@@ -0,0 +1,114 @@
+/*!
+ * Page Check (v1.0.0.20180225), http://tpkn.me/
+ */
+
+const path = require('path');
+const phantomjs = require('phantom');
+
+
+/**
+ * Check for repeating errors
+ * 
+ * @param  {Array}  list
+ * @param  {String}  error
+ * @return {Boolean}
+ */
+function isClone(list, error){
+   for(let i = 0, len = list.length; i < len; i++){
+      if(list[i].details == error){
+         return true;
+      }
+   }
+   return false;
+}
+
+function PageCheck(link, options, timeout = 60){
+   return new Promise((resolve, reject) => {
+      let rid, aid, phantom, page, errors = [];
+
+      let no_clones = typeof options.no_clones !== 'undefined' ? options.no_clones : false;
+
+      if(typeof options === 'number'){
+         timeout = options;
+      }
+
+      phantomjs.create()
+      .then(ph => {
+         phantom = ph;
+         return phantom.createPage();
+      })
+      .then(pg => {
+         page = pg;
+
+         // Js error
+         page.on('onError', (err, trace) => {
+            if(trace && trace.length){
+               trace.forEach(t => {
+                  if(!no_clones || !isClone(errors, err)){
+                     errors.push({code: 1, type: 'js error', details: err, line: 'line ' + t.line});
+                  }
+               });
+            }
+         });
+
+         // Missing files
+         page.on('onResourceError', (resourceError) => {
+            errors.push({code: 2, type: 'missing file', details: resourceError.errorString, line: ''});
+         });
+
+         // Console
+         page.on('onConsoleMessage', (msg, lineNum, sourceId) => {
+            if(!no_clones || !isClone(errors, msg)){
+               errors.push({code: 3, type: 'console', details: msg, line: typeof lineNum === 'undefined' ? '' : lineNum});
+            }
+         });
+
+         // External files
+         page.on('onResourceRequested', (requestData, networkRequest) => {
+            if(requestData.url.indexOf(link) == -1){
+               errors.push({code: 4, type: 'external request', details: requestData.url, line: ''});
+            }
+         });
+
+         return page.open(link);
+      })
+      .then(status => {
+         if(status === 'fail'){
+            phantom.exit();
+            reject({status: 'fail', message: 'can\'t open ' + link});
+            return;
+         }
+
+         // Some error could be inside 'resize' handler
+         rid = setInterval(function(){
+            let size = Math.floor(Math.random() * 1000);
+            page.property('viewportSize', { width: size, height: size });
+         }, timeout * 100);
+
+
+         // Lets wait for a while...
+         aid = setTimeout(() => {
+            clearInterval(rid);
+            phantom.exit(0);
+
+            errors.sort((a, b) => a.code - b.code);
+            
+            if(typeof options.filter === 'function'){
+               errors = errors.filter(options.filter);
+            }
+
+            resolve({status: 'done', link: link, errors: errors});
+
+         }, timeout * 1000);
+      })
+      .catch(err => {
+         clearTimeout(aid);
+         clearInterval(rid);
+         phantom.exit(1);
+
+         reject({status: 'fail', message: err.message});
+      });
+   });
+}
+
+module.exports = PageCheck;
